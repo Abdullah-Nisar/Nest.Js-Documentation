ee3adb88d688c55ef6ac8ba0b79bdb6c9e208dd7
Abdullah-Nisar <abdullah.bscs20seecs@seecs.edu.pk>
2023-07-13 01:19:54 +0500

    Pipes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus, HttpException, BadRequestException, UseFilters, ForbiddenException, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus, HttpException, BadRequestException, UseFilters, ForbiddenException, ParseIntPipe, Query, UsePipes } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 import {Cat} from './interfaces/cat.interface';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
-
+import { JoiValidationPipe } from 'src/joi-validation.pipe';
+import { createCatSchema } from 'src/cats/dto/cat.dto';
+import { ValidationPipe } from 'src/validation.pipe';
 
 // import {Response} from 'express';
 
@@ -14,42 +16,52 @@ import { HttpExceptionFilter } from 'src/http-exception.filter';
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
-  @Post()
+  // @Post()
   // async create(@Body() createCatDto: CreateCatDto) {
   //   // this.catsService.create(createCatDto);
   //   return 'This action adds a new cat';
   // }
 
-  
-  async create(@Body() createCatDto: CreateCatDto) {
-    throw new ForbiddenException();
-  }
+  // @Post()
+  // async create(@Body() createCatDto: CreateCatDto) {
+  //   throw new ForbiddenException();
+  // }
+
   // @Post()
   // create(@Res({passthrough: true}) res: Response){
   //   res.status(HttpStatus.CREATED).send();
   // }
 
+  // @Post()
+  // @UsePipes(new JoiValidationPipe(createCatSchema))
+  // async create(@Body() createCatDto: CreateCatDto) {
+  //   this.catsService.create(createCatDto);}
+
+  @Post()
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);}
+
+
+
   @Get()
   // async findAll(): Promise<Cat[]> {
   //   return this.catsService.findAll();
   // }
-  async findAll(){
-    throw new HttpException('Hello Cat', HttpStatus.FORBIDDEN);
-  }
+  
   // async findAll(){
   //   throw new ForbiddenException();
   // }
 
-  // async findAll(){
-  //   try{
-  //     await this.catsService.findAll();
-  //   }catch(e){
-  //     throw new HttpException({status: HttpStatus.FORBIDDEN,error: 'This is a custom message'},
-  //      HttpStatus.FORBIDDEN,{
-  //       cause: e
-  //     });
-  //   }
-  // }
+  async findAll(){
+    try{
+      await this.catsService.findAll();
+    }catch(e){
+      throw new HttpException({status: HttpStatus.FORBIDDEN,error: 'This is a custom message'},
+       HttpStatus.FORBIDDEN,{
+        cause: e
+      });
+    }
+  }
 
   // async findAll(){
   //   try{
@@ -62,20 +74,18 @@ export class CatsController {
 
 
   @Get(':id')
+  async findOne(@Param('id', new ParseIntPipe({errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE})) id: number) {
+    return this.catsService.findOne(id);
+  }
   // findOne(@Param('id') id: string) {
   //   return this.catsService.findOne(+id);
   // }
-  async findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.catsService.findOne(id);
-  }
+  // async findOne(@Param('id', ParseIntPipe) id: number) {
+  //   return this.catsService.findOne(id);
+  // }
+  // @Get()
+  // async findOne(@Query('id', ParseIntPipe) id: number) {
+  //   return this.catsService.findOne(id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return this.catsService.update(+id, updateCatDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.catsService.remove(+id);
-  }
 }
--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCatDto {}
+// export class CreateCatDto {
+//     name: string;
+//     age: number;
+//     breed: string;    
+// }
+
+
+import { IsString, IsInt } from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  age: number;
+
+  @IsString()
+  breed: string;
+}
